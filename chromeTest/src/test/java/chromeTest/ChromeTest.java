5fe7d6a61c8eb922cda15d3c35443dfaca273c24
K.A_MOHAMMED <K.A_MOHAMMED@MOHAMMED>
2019-04-08 12:27:03 +0530

    chrome to firefox

--- chromeTest/src/test/java/chromeTest/ChromeTest.java
+++ chromeTest/src/test/java/chromeTest/ChromeTest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class ChromeTest 
@@ -11,7 +12,7 @@ public class ChromeTest
 	@Test
 	public void chrometest() throws InterruptedException
 	{
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		Thread.sleep(4000);
